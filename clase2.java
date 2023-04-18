83ef2e95c5b434443ba922d9a92fbdc46c5541e9
¨Rafael <humer-merlin@hotmail.com>
2023-04-17 23:50:19 -0600

    ya veo

--- clase2.java
+++ clase2.java
@@ -3,5 +3,8 @@ import javax.sound.sampled.SourceDataLine;
 public class clase2 {
     public static void main(String[] args) {
         System.out.println("pendejadas");
+        System.out.println("pendejadas 1");
+        System.out.println("pendejadas 3");
+        System.out.println("pendejadas 4");
     }
 }
