0c46be1be24ed1e2fc3617dcf35acbe428d0ecf7
¨Rafael <humer-merlin@hotmail.com>
2023-04-17 23:18:20 -0600

    agregando cp

--- clase2.java
+++ clase2.java
@@ -1,5 +1,11 @@
 public class clase2 {
     public static void main(String[] args) {
         System.out.println("sexo gat");
+        int x = 2;
+        if (x == 2) {
+            System.out.println("x es " + x);
+            System.out.println("efw");
+        }
     }
+
 }
